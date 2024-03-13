21bef9d9282ef41d30d44454aae3f4944071b7d5
Kerwin <58503913+KIM-JONG-TAEK@users.noreply.github.com>
2024-03-13 16:19:32 +0900

    commit

---
+++ RNCourse/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text>Help Me!!</Text>
+      </View>
+      <Text>I want to go home!!</Text>
+      <Button title='Tap me!' />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
